f2eb0fe834207cd53a0eb5832ad6b1f4b25aea55
chandanritvik <chandan.gurramluck@gmail.com>
2019-03-31 18:44:30 +0530

    Update insert.sql

--- insert.sql
+++ insert.sql
@@ -3,10 +3,10 @@ insert into movie values (NULL,"LatestMovie2","3:30:00","Hindi");
 insert into movie values (NULL,"LatestMovie3","2:15:00","German");
 insert into movie values (NULL,"LatestMovie4","1:30:00","Japaneese");
 
-insert into cast_and_crew values (NULL,"Actor1","1980:10:05","very great actor from childhood");
-insert into cast_and_crew values (NULL,"Actor2","1982:08:08","average actor from childhood");
-insert into cast_and_crew values (NULL,"Actor3","1970:09:13","dummy actor");
-insert into cast_and_crew values (NULL,"Actor4","1960:10:05","very famous");
+insert into cast_and_crew values (1,"Actor1","1980:10:05","very great actor from childhood");
+insert into cast_and_crew values (2,"Actor2","1982:08:08","average actor from childhood");
+insert into cast_and_crew values (3,"Actor3","1970:09:13","dummy actor");
+insert into cast_and_crew values (4,"Actor4","1960:10:05","very famous");
 
 insert into watchlist values ("chandan.gurramluck@gmail.com",1);
 insert into watchlist values ("chandan.gurramluck@gmail.com",2);
