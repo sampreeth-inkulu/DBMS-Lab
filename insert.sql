835f3f6158432ad326c7dbee5ebbc00877b803ed
chandanritvik <chandan.gurramluck@gmail.com>
2019-03-31 18:31:13 +0530

    Add files via upload

---
+++ insert.sql
@@ -0,0 +1,11 @@
+insert into movie values (NULL,"LatestMovie1","2:30:00","English");
+insert into movie values (NULL,"LatestMovie2","3:30:00","Hindi");
+insert into movie values (NULL,"LatestMovie3","2:15:00","German");
+insert into movie values (NULL,"LatestMovie4","1:30:00","Japaneese");
+
+insert into cast_and_crew values (NULL,"Actor1","1980:10:05","very great actor from childhood");
+insert into cast_and_crew values (NULL,"Actor2","1982:08:08","average actor from childhood");
+insert into cast_and_crew values (NULL,"Actor3","1970:09:13","dummy actor");
+insert into cast_and_crew values (NULL,"Actor4","1960:10:05","very famous");
+
+
