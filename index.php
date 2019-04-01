9bdb2fb9d0f669db3e6eb9f01867c3a2063b143c
sampreeth1999 <32814049+sampreeth1999@users.noreply.github.com>
2019-04-01 13:06:12 +0000

    Add files via upload

--- index.php
+++ index.php
@@ -9,9 +9,36 @@
 	<center>
 		MoviePedia</br></br>
 		<h3>Login</h3>
-		<form action = "login-backend.php" method = "POST">
-			<input name = "email" type = "email" placeholder = "Email"><br><br>
-			<input name = "password" type = "password" placeholder = "Password"><br><br>
+		<?php
+			if ($_SERVER["REQUEST_METHOD"] == "POST") {
+				require_once 'dbconnect.php';
+
+				$email = $_POST["email"];
+				$password = $_POST["password"];
+
+				$password = md5($password);
+
+				$query = "SELECT * FROM User WHERE email='$email' AND password='$password'";
+
+
+				$result = mysqli_query($con, $query);
+				$row=mysqli_fetch_array($result);
+
+				$numResults = mysqli_num_rows($result);
+
+				if($numResults == 1){
+					session_start();
+					$_SESSION['email'] = $email;
+					header("Location: user.php");
+					exit();
+				}else{	/* Expected num_results = 0 */
+					echo "<h4>Invalid credentials!</h4>";
+				}
+			}
+		?>
+		<form action = "<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" method = "POST">
+			<input name = "email" type = "email" placeholder = "Email" required><br><br>
+			<input name = "password" type = "password" placeholder = "Password" required><br><br>
 			<button type = "submit">Submit</button>
 		</form>
 		</br>
