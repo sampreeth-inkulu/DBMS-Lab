a97b125e7ede61271b190bb74aeda7c2b95c0561
chandanritvik <chandan.gurramluck@gmail.com>
2019-04-01 19:12:30 +0530

    Update allmovies.php

--- allmovies.php
+++ allmovies.php
@@ -11,6 +11,7 @@
 		<button type = "submit" name = "Logout">Logout</button>
 	</form>
 <?php
+	
 	session_start();
 	echo "<h2>Hello, User!</h2></br>";
 	echo "Email: ".$_SESSION['email']."</br>";
@@ -22,12 +23,16 @@
 	echo "|"."Language</br>";
 if ($result=mysqli_query($con,$sql))
   {
+	echo "<table> <tr> <th>Movie Name</th> <th>Duration(hh:mm:ss)</th> <th>Language</th> </tr>";
   while ($obj=mysqli_fetch_object($result))
     {
-    echo $obj->title."\t".$obj->duration."\t".$obj->lang."</br>";
+		echo "<tr> <td>$obj->title</td> <td>$obj->duration</td> <td>$obj->lang</td> </tr>";
+   // echo $obj->title."\t".$obj->duration."\t".$obj->lang."</br>";
     }
+	echo "</table>";
   // Free result set
   mysqli_free_result($result);
+  
 }
 
 ?>
