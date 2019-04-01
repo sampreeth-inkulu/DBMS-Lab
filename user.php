f783f751e307f15c4982566b1a59a695291a2c01
chandanritvik <chandan.gurramluck@gmail.com>
2019-04-01 19:24:14 +0530

    Update user.php

--- user.php
+++ user.php
@@ -9,10 +9,24 @@
 	<form name = "logoutForm" action = "logout.php" method = "POST" align = "right">
 		<button type = "submit" name = "Logout">Logout</button>
 	</form>
+	
+	<form name = "watchlist" action = "watchlist.php" method = "POST" align = "list">
+		<button type = "submit" name = "watchlist">To Watch Now!!!</button>
+	</form>
+	
 <?php
 	session_start();
 	echo "<h2>Hello, User!</h2></br>";
 	echo "Email: ".$_SESSION['email']."</br>";
 ?>
+
+	<form name = "allMovieList" action = "allmovies.php" method = "POST" align = "left">
+		<button type = "submit" name = "allmovies">View all the Movies</button>
+	</form>
+	
+	<form name = "allActorList" action = "allactors.php" method = "POST" align = "left">
+		<button type = "submit" name = "allActors">View all the Actors</button>
+	</form>
+	
 </body>
 </html>
