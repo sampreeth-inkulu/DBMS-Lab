a17663f5c8a52d5a082616f30e9c142ea2fb3dc9
sunnydie86 <33957578+sunnydie86@users.noreply.github.com>
2019-04-02 10:34:32 +0530

    Update celeb.php

--- celeb.php
+++ celeb.php
@@ -22,7 +22,52 @@
 	$id = $_GET['id'];
 	
 	require_once'dbconnect.php';
+	$query = "SELECT * FROM cast_and_crew WHERE  id ='$id'";
+	$result = mysqli_query($con, $query);
+	$row = mysqli_fetch_array($result);
+
+	echo "<h2>".$row['name']."</h2>";	
+	echo "<h5>date of birth-".$row['date_of_birth']."</h5>";	
+	echo "<h5>".$row['bio']."</h5>";	
+
+	$query = "SELECT count(*) as cnt FROM stanlist WHERE  id ='$id'";
+	$result = mysqli_query($con, $query);
+	$row = mysqli_fetch_array($result);
+	echo mysqli_error($con);
+	echo"<h5>followed by ".$row['cnt']."</h5>";
 	
+	$query = "SELECT * FROM part_of WHERE  id ='$id'";
+	$results = mysqli_query($con, $query);
+	if($results)
+	{
+		echo '<table>
+			<tr>
+				<th>title</th>
+				<th>role</th>
+			</tr>';
+		while ($row = mysqli_fetch_array($results)){
+			$id = $row['movie_id'];
+			$role = $row['role'];
+			$query = "SELECT * FROM movie WHERE movie_id ='$id'";
+			$result = mysqli_query($con, $query);
+			$row = mysqli_fetch_array($result);
+			
+			echo '
+				<tr>
+					<td>'.$row['title'].'</td>	
+					<td>'.$role.'</td>
+					<td>
+						<a href="movie.php?id=',urlencode($id),'">
+							<div style="height:100%;width:100%">
+								view
+							</div>
+						</a>
+					</td>
+				</tr>';
+		}
+		echo '
+		</table>';
+	}				
 	
 ?>
 </body>
