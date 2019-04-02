479b30be1cae3cee36af7b5324924740abd2656f
sampreeth1999 <32814049+sampreeth1999@users.noreply.github.com>
2019-04-02 06:02:50 +0000

    Added watchlist

--- movie.php
+++ movie.php
@@ -13,22 +13,69 @@
 	
 <?php
 	session_start();
-	if(!isset($_SESSION['email'])) {
+	if(! isset($_SESSION['email'])) {
 		header("Location: index.php");
 		exit();
 	}
+	echo "<h2>Hello, User!</h2></br>";
+	echo "<a href = 'user.php'>My home</a></br>";
+	echo "Email: ".$_SESSION['email']."</br>";
 
 	require_once'dbconnect.php';
 	
 	$id = $_GET['id'];
-	$query = "SELECT * FROM movie WHERE  movie_id ='$id'";
-	$result = mysqli_query($con, $query);
+	$query = "SELECT * FROM Movie WHERE  movie_id ='$id'";
+    $result = mysqli_query($con, $query);
+    // echo mysqli_error($con);
 	$row = mysqli_fetch_array($result);
 
 	echo "<h2>".$row['title']."</h2>";	
-	echo "<h5>language-".$row['lang']."</h5>";	
-	echo "<h5>duration-".$row['duration']."</h5>";			
-	
+	echo "<h5>Language-".$row['lang']."</h5>";	
+    echo "<h5>Duration-".$row['duration']."</h5>";
+
+    if(isset($_GET['add'])) {
+        if($_GET['add'] == "yes") {
+            $email = $_SESSION['email'];
+			$query = "SELECT * FROM Watchlist WHERE movie_id = '$id' AND email = '$email'";
+            $result = mysqli_query($con, $query);
+            if(mysqli_num_rows($result) == 0) {
+                $email = $_SESSION['email'];
+                $query = "INSERT INTO Watchlist VALUES ('$id','$email')";
+                if(mysqli_query($con, $query)) {
+                    // echo "<br><a href = 'movie.php?id=".urlencode($id)."&add=".urlencode("no")."'>Remove from watchlist</a>";
+                }else {
+                    echo "<br>Error:".mysqli_error($con);
+                }
+            }else {
+                // echo "<br><a href = 'movie.php?id=".urlencode($id)."&add=".urlencode("no")."'>Remove from watchlist</a>";
+            }
+        }else if($_GET['add'] == "no"){
+            $email = $_SESSION['email'];
+			$query = "SELECT * FROM Watchlist WHERE movie_id = '$id' AND email = '$email'";
+            $result = mysqli_query($con, $query);
+            if(mysqli_num_rows($result) != 0) {
+                $email = $_SESSION['email'];
+                $query = "DELETE FROM Watchlist WHERE movie_id = '$id' AND email = '$email'";
+                if(mysqli_query($con, $query)) {
+                    // echo "<br><a href = 'movie.php?id=".urlencode($id)."&add=".urlencode("yes")."'>Add to watchlist</a>";
+                }else {
+                    echo "<br>Error:".mysqli_error($con);
+                }
+            }else {
+                // echo "<br><a href = 'movie.php?id=".urlencode($id)."&add=".urlencode("yes")."'>Add to watchlist</a>";
+            }
+        }
+    }
+    
+    $email = $_SESSION['email'];
+    $query = "SELECT * FROM Watchlist WHERE movie_id = '$id' AND email = '$email'";
+    $result = mysqli_query($con, $query);
+    if(mysqli_num_rows($result) == 0) {
+        echo "<br><a href = 'movie.php?id=".urlencode($id)."&add=".urlencode("yes")."'>Add to watchlist</a>";
+    }else {
+        echo "<br><a href = 'movie.php?id=".urlencode($id)."&add=".urlencode("no")."'>Remove from watchlist</a>";
+    }
+
 	$query = "SELECT * FROM part_of WHERE  movie_id ='$id'";
 	$results = mysqli_query($con, $query);
 	if($results)
